b8568238424c33f2a56d8de8421f1e3a20f98326
QuBerto <roberto@guagliardo.nl>
2024-03-25 20:52:35 +0100

    init release

---
+++ class-custom-shipping.php
@@ -0,0 +1,337 @@
+<?php
+
+
+/**
+ * WC_Shipping_SelectCourier class.
+ *
+ * @class         WC_Shipping_SelectCourier
+ * @version        1.0.0
+ * @package        Shipping-for-WooCommerce/Classes
+ * @category    Class
+ * @author         Roberto van SitiWeb
+ */
+class WC_Shipping_SelectCourier extends WC_Shipping_Method {
+
+    /**
+     * Constructor. The instance ID is passed to this.
+     */
+    public function __construct( $instance_id = 0 ) {
+        $this->id                    = 'select_courier';
+        $this->instance_id           = absint( $instance_id );
+        $this->method_title          = __( 'select_courier Method' );
+        $this->method_description    = __( 'select_courier method for demonstration purposes.' );
+        $this->supports              = array(
+            'shipping-zones',
+            'instance-settings',
+        );
+        $this->instance_form_fields = array(
+            'enabled' => array(
+                'title'         => __( 'Enable/Disable' ),
+                'type'             => 'checkbox',
+                'label'         => __( 'Enable this shipping method' ),
+                'default'         => 'yes',
+            ),
+            'title' => array(
+                'title'         => __( 'Method Title' ),
+                'type'             => 'text',
+                'description'     => __( 'This controls the title which the user sees during checkout.' ),
+                'default'        => __( 'Tyche Shipping Method' ),
+                'desc_tip'        => true
+            )
+        );
+        $this->enabled              = $this->get_option( 'enabled' );
+        $this->title                = $this->get_option( 'title' );
+
+        add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+    }
+
+    public static function woocommerce_shipping_methods( $methods ) {
+        $methods[] = 'Custom_Shipping'; return $methods;
+    }
+    
+
+    /**
+    * calculate_shipping function.
+    * @param array $package (default: array())
+    */
+    public function calculate_shipping( $package = array() ) {
+        $options = $this->get_shipping_options();
+
+        // Check if shipping options are available
+        if (!empty($options)) {
+            foreach ($options as $option) {
+                // Add rate for each shipping option
+                $this->add_rate( array(
+                    'id'    => $this->id . '_' . $option['id'], // Unique ID for the rate
+                    'label' => $option['name'], // Displayed label for the rate
+                    'cost'  => $option['price'], // Shipping cost for the option
+                ) );
+            }
+        } else {
+            // If no shipping options are available, you can handle it accordingly
+            $this->add_rate( array(
+                'id'    => $this->id . '_no_option',
+                'label' => 'No shipping options available',
+                'cost'  => 0,
+            ) );
+        }
+
+   
+    }
+
+    private function get_shipping_options() {
+        // Start time
+        $start_time = microtime(true);
+
+        // Construct the API request data
+        $request_data = $this->construct_api_request_data();
+
+        // Generate a hash of the request data
+        $request_hash = md5(serialize($request_data));
+
+        // Check if the cached result exists
+        $cached_result = get_transient('select_courier_shipping_options_' . $request_hash);
+
+        if ($cached_result !== false) {
+            // End time
+            $end_time = microtime(true);
+
+            // Calculate the time taken in milliseconds
+            $time_taken_ms = ($end_time - $start_time) * 1000;
+
+            // Log the time taken in the error log
+            error_log('Fetching shipping options from cache took ' . $time_taken_ms . ' ms');
+
+            return $cached_result; // Return the cached result
+        } else {
+            // Make the API request
+            $response = $this->make_api_request($request_data);
+
+            // Process the API response and extract shipping options
+            $shipping_options = $this->process_api_response($response);
+
+            // Cache the shipping options with the hashed key
+            set_transient('select_courier_shipping_options_' . $request_hash, $shipping_options, HOUR_IN_SECONDS); // Cache for 1 hour
+
+            // End time
+            $end_time = microtime(true);
+
+            // Calculate the time taken in milliseconds
+            $time_taken_ms = ($end_time - $start_time) * 1000;
+
+            // Log the time taken in the error log
+            error_log('Fetching shipping options from API took ' . $time_taken_ms . ' ms');
+
+            return $shipping_options;
+        }
+    }
+
+
+    private function construct_api_request_data() {
+        // Construct and return the API request data based on cart contents and shipping address
+        // Include all necessary details required for the API request
+        // Construct the API request
+        $shipping_address = WC()->customer->get_shipping();
+      
+        $items = array();
+
+        $default_country = get_option('woocommerce_default_country');
+        if ($default_country){
+            $country_parts = explode(':', $default_country);
+        }
+        
+
+        if (isset($country_parts) && isset($country_parts[0])){
+            $store_country = $country_parts[0];
+        }
+        
+        if (isset($store_country) && $store_country){
+            $country = get_option('selectcourier_origin_country', $store_country);
+        }
+        else{
+            $country = get_option('selectcourier_origin_country', false);
+        }
+        foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+            // Fetch product data
+            $product = $cart_item['data'];
+
+            // Get product dimensions if available
+            $weight = $product->get_weight() ?: 0.1;
+            $height = $product->get_height() ?: 0;
+            $width = $product->get_width() ?: 0;
+            $length = $product->get_length() ?: 0;
+            
+            $product = $cart_item['data'];
+
+            // Fetching required data for each product
+            $quantity = $cart_item['quantity'];
+            $product_code = $product->get_sku(); // Assuming SKU is used as product code
+            $o_country = 'NL';// $country; // Assuming NL (Netherlands) as the country of origin
+            $weight = $product->get_weight() ?: 0.1; // Weight of the product
+            $description = $product->get_name(); // Product name as description
+            $value = $product->get_price(); // Product price
+            $value_currency = get_woocommerce_currency(); // Get the currency set in WooCommerce
+            $hs_code = ''; // Harmonized System Code
+
+            // Creating an array for each product
+            $product_data = array(
+                'quantity' => $quantity,
+                'product_code' => $product_code,
+                'o_country' => $o_country,
+                'weight' => $weight,
+                'description' => $description,
+                'value' => $value,
+                'value_currency' => $value_currency,
+                'hs_code' => $hs_code
+            );
+
+            // Adding the product data to the products array
+            $products[] = $product_data;
+
+         
+        }
+        // Add item to the items array
+        $item = array(
+            'weight'    => $weight, 
+            'height'    => $height,
+            'width'     => $width,
+            'length'    => $length,
+            'contents'  => 'equipment',
+            //'products' => $products
+            
+        );
+        $items[] = $item;
+
+        $environment = get_option('selectcourier_environment','development');
+        $username = get_option('selectcourier_username', false);
+        $password = get_option('selectcourier_password', false);
+
+        $name = get_option('selectcourier_origin_name',false);
+        $phone = get_option('selectcourier_origin_phone', false);
+        $email = get_option('selectcourier_origin_email',  get_option('admin_email', false));
+        $street = get_option('selectcourier_origin_street',  get_option('woocommerce_store_address', false));
+        $postcode = get_option('selectcourier_origin_postal', get_option('woocommerce_store_postcode', false));
+        $city = get_option('selectcourier_origin_city',  get_option('woocommerce_store_city', false));
+
+        
+        
+
+        if (!$username || !$password){
+            error_log('SelectCourier: Missing password or username');
+            return false;
+        }
+        
+        if (!$name){
+            error_log('SelectCourier: Missing contact name');
+            return false;
+        }
+
+        if (!$phone){
+            error_log('SelectCourier: Missing phone ');
+            return false;
+        }
+
+        if (!$email){
+            error_log('SelectCourier: Missing email ');
+            return false;
+        }
+
+        if (!$street){
+            error_log('SelectCourier: Missing street ');
+            return false;
+        }
+
+        if (!$postcode){
+            error_log('SelectCourier: Missing postcode ');
+            return false;
+        }
+
+        if (!$city){
+            error_log('SelectCourier: Missing city ');
+            return false;
+        }
+
+        if (!$country){
+            error_log('SelectCourier: Missing country ');
+            return false;
+        }
+
+        $request = array(
+            "environment"   => $environment,
+            "username"      => $username,
+            "password"      => $password,
+            "action"        => "quote",
+            "shipment"      => array(
+                "o_name"        => $name,
+                "o_phone"       => $phone,
+                "o_email"       => $email,
+                "o_street_1"    => $street,
+                "o_postal"      => $postcode,
+                "o_city"        => $city,
+                "o_country"     => $country,
+                "d_name"        => "Recipient",
+                "d_phone"       => $phone,
+                "d_street_1"    => $shipping_address['address_1'],
+                "d_postal"      => $shipping_address['postcode'],
+                "d_city"        => $shipping_address['city'],
+                "d_country"     => $shipping_address['country'],
+                "type"          => "parcel",
+                "contents"      => "Radio Equipment",
+                "value"         => "parcel",
+                "return_service"=> 0,
+                "items"         => $items // Include items in the shipment
+            )
+        );
+        error_log(print_r($request,1));
+        return $request;
+    }
+
+    private function make_api_request($request) {
+        
+        // Start time
+        $start_time = microtime(true);
+
+        // Make the API request using cURL
+        $url = "https://www.selectcourier.com/api/json";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        // End time
+        $end_time = microtime(true);
+
+        // Calculate the time taken in milliseconds
+        $time_taken_ms = ($end_time - $start_time) * 1000;
+
+        // Log the time taken in the error log
+        error_log('API request took ' . $time_taken_ms . ' ms');
+        $response = json_decode($response, true);
+        error_log(print_r($response,1));
+        return $response;
+        
+    }
+
+    private function process_api_response($response) {
+        $shipping_options = [];
+        if (isset($response["result"]["services"])) {
+            foreach ($response["result"]["services"] as $method) {
+                if (!empty($method["courier_name"])) {
+                    $shipping_options[] = array(
+                        'id'    => $method["service_id"],
+                        'name'  => $method["courier_name"]. ' ' . $method['service_name'],
+                        'price' => $method["total_price"]
+                    );
+                }
+            }
+        }
+
+        return $shipping_options;
+    }
+
+}
+?>
---
+++ selectcourier.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Plugin Name: Maatwerk Select courier
+ * Description: Maatwerk Select courier voor profmbroadcast.nl
+ * Version: 1.0
+ * Author: Roberto van SitiWeb
+ * Author URI: http://yourwebsite.com/
+ */
+
+add_action('plugins_loaded', 'load_plugins', 0);
+function load_plugins() {
+    // Check if WooCommerce is active and the WC_Shipping_Method class exists
+    if ( class_exists( 'WC_Shipping_Method' ) ) {
+        require_once( plugin_dir_path( __FILE__ ) . 'class-custom-shipping.php' );
+    } else {
+        // Optionally, handle the case where WooCommerce is not active
+        // For example, you could deactivate your plugin or display a notice
+        add_action( 'admin_notices', 'my_custom_plugin_admin_notice' );
+    }
+}
+// Function to display an admin notice if WooCommerce is not active
+function my_custom_plugin_admin_notice() {
+    ?>
+    <div class="notice notice-warning">
+        <p><?php _e( 'My Custom Plugin requires WooCommerce to be installed and active.', 'my-custom-plugin-text-domain' ); ?></p>
+    </div>
+    <?php
+}
+
+add_filter('woocommerce_shipping_methods', array('WC_Shipping_SelectCourier','woocommerce_shipping_methods' ));
+
+add_filter( 'woocommerce_shipping_methods', 'register_select_courier' );
+
+function register_select_courier( $methods ) {
+
+    // $method contains available shipping methods
+    $methods[ 'select_courier' ] = 'WC_Shipping_SelectCourier';
+
+    return $methods;
+}
+
+// Add a custom tab to WooCommerce settings
+add_filter('woocommerce_settings_tabs_array', 'add_shipping_options_tab', 50);
+function add_shipping_options_tab($tabs) {
+    $tabs['shipping_options'] = __('Shipping Options', 'woocommerce');
+    return $tabs;
+}
+
+// Add fields to the custom tab
+add_action('woocommerce_settings_tabs_shipping_options', 'shipping_options_tab');
+function shipping_options_tab() {
+    woocommerce_admin_fields(array_merge(shipping_options_fields(), origin_address_fields()));
+}
+
+// Define fields for the custom tab
+function shipping_options_fields() {
+    $fields = array(
+        'shipping_options_title' => array(
+            'name' => __('Selectcourier opties', 'woocommerce'),
+            'type' => 'title',
+            'desc' => '',
+            'id' => 'shipping_options_title'
+        ),
+        'selectcourier_environment' => array(
+            'name' => __('Selectcourier Environment', 'woocommerce'),
+            'type' => 'select',
+            'desc' => __('Select the environment', 'woocommerce'),
+            'id' => 'selectcourier_environment',
+            'options' => array(
+                'production' => __('Production', 'woocommerce'),
+                'development' => __('Development', 'woocommerce')
+            )
+        ),
+        'selectcourier_username' => array(
+            'name' => __('Username', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the username', 'woocommerce'),
+            'id' => 'selectcourier_username'
+        ),
+        'selectcourier_password' => array(
+            'name' => __('Password', 'woocommerce'),
+            'type' => 'password',
+            'desc' => __('Enter the password', 'woocommerce'),
+            'id' => 'selectcourier_password'
+        ),
+        'selectcourier_username' => array(
+            'name' => __('Username', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the username', 'woocommerce'),
+            'id' => 'selectcourier_username'
+        ),
+        'shipping_options_section_end' => array(
+            'type' => 'sectionend',
+            'id' => 'shipping_options_section_end'
+        )
+    );
+    return $fields;
+}
+
+
+
+// Define fields for the origin address section
+function origin_address_fields() {
+    $fields = array(
+        'origin_address_title' => array(
+            'name' => __('Origin Address', 'woocommerce'),
+            'type' => 'title',
+            'desc' => '',
+            'id' => 'origin_address_title'
+        ),
+        'selectcourier_origin_name' => array(
+            'name' => __('Name', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the name', 'woocommerce'),
+            'id' => 'selectcourier_origin_name'
+        ),
+        'selectcourier_origin_phone' => array(
+            'name' => __('Phone', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the phone number', 'woocommerce'),
+            'id' => 'selectcourier_origin_phone'
+        ),
+        'selectcourier_origin_email' => array(
+            'name' => __('Email', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the email address', 'woocommerce'),
+            'id' => 'selectcourier_origin_email'
+        ),
+        'selectcourier_origin_street' => array(
+            'name' => __('Street', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the street address', 'woocommerce'),
+            'id' => 'selectcourier_origin_street'
+        ),
+        'selectcourier_origin_postal' => array(
+            'name' => __('Postal Code', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the postal code', 'woocommerce'),
+            'id' => 'selectcourier_origin_postal'
+        ),
+        'selectcourier_origin_city' => array(
+            'name' => __('City', 'woocommerce'),
+            'type' => 'text',
+            'desc' => __('Enter the city', 'woocommerce'),
+            'id' => 'selectcourier_origin_city'
+        ),
+        'selectcourier_origin_country' => array(
+            'name' => __('Country', 'woocommerce'),
+            'type' => 'select',
+            'options' => WC()->countries->get_countries(),
+            'desc' => __('Select the country', 'woocommerce'),
+            'id' => 'selectcourier_origin_country'
+        ),
+        
+        'origin_address_section_end' => array(
+            'type' => 'sectionend',
+            'id' => 'origin_address_section_end'
+        )
+    );
+    return $fields;
+}
+
+add_action('woocommerce_update_options', 'save_shipping_options');
+function save_shipping_options() {
+    woocommerce_update_options(shipping_options_fields());
+    woocommerce_update_options(origin_address_fields()); // Save origin address fields
+}
