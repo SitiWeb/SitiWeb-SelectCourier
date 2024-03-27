c69c4df37f879b1cf8ab7f6dda8c6ae73f3d1247
roberto Guagliardo <administratie@sitiweb.nl>
2024-03-27 17:27:18 +0100

    Update version number

--- selectcourier.php
+++ selectcourier.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Maatwerk Select courier
  * Description: Maatwerk Select courier voor profmbroadcast.nl
- * Version: 1.3.2
+ * Version: 1.3.3
  * Author: Roberto van SitiWeb
  * Author URI: https://sitiweb.nl/
  */
